fac62f3486ae722ea7044d8856fd71925306701a
Bytemagic <bytemagic@yandex.ru>
2026-01-15 15:48:00 +0300

    Add alamofire

---
+++ Financial/Statics/WebRequestWrapper.swift
@@ -0,0 +1,22 @@
+//
+//  WebRequestWrapper.swift
+//  Financial
+//
+//  Created by Roman on 15.01.2026.
+//
+
+import Foundation
+
+class WebRequestWrapper
+{
+    static let instance = WebRequestWrapper()
+    
+    func CreateRequestCurrency() -> String
+    {
+        var currentCurrency = ""
+        
+        
+        return currentCurrency
+    }
+    
+}
--- Financial/ViewControllers/PiggyBankViewController.swift
+++ Financial/ViewControllers/PiggyBankViewController.swift
@@ -36,7 +36,24 @@ class PiggyBankViewController: UIViewController {
         viewActivator(acivated: true)
         checkButtons()
         
-        titleLabel.text = String(localized: "MyPiggies")
+        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { print("Нет данных"); return }
+            if let json = try? JSONSerialization.jsonObject(with: data) as? [String: Any],
+               let valute = json["Valute"] as? [String: Any],
+               let usd = valute["USD"] as? [String: Any],
+               let value = usd["Value"] as? Double {
+                print("Официальный курс USD к RUB:", value)
+                DispatchQueue.main.async {
+                    self.titleLabel.text = "\(value)"
+                }
+            } else {
+                print("Не получилось обработать ответ")
+            }
+        }
+        task.resume()
+       
+       // titleLabel.text = String(localized: "MyPiggies")
         
         addNewTargetButton.setTitle(String(localized: "AddNewTargetButton"),for: .normal)
         
